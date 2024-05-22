ed59f3359b2a93f6c580cdae4e9409d515e724f7
biongodev <biongo.dev@gmail.com>
2024-05-22 14:50:39 +0900

    feat(all): setup clerk

---
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Dashboard() {
+  return (
+    <Button variant={"destructive"} size={"lg"}>
+      test
+    </Button>
+  );
+}
