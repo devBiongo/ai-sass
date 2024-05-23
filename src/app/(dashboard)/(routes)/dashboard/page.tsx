8145ebf430e189d8287db3289ebb13c7b65b0bad
biongodev <biongo.dev@gmail.com>
2024-05-23 14:23:44 +0900

    feat(all): add sidebar

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Dashboard() {
-  return (
-    <Button variant={"destructive"} size={"lg"}>
-      test
-    </Button>
-  );
-}
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,3 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Page() {
-  return <div>Landing page unprotected</div>;
+  return (
+    <div>
+      Landing page unprotected
+      <Link href={"/sign-in"}>
+        <Button>Login</Button>
+      </Link>
+      <Link href={"/sign-up"}>
+        <Button>register</Button>
+      </Link>
+    </div>
+  );
 }
